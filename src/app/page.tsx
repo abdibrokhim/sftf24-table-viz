27c451f633b709b9ee2ab794a18182273b117e9b
abdibrokhim <abdibrokhim@gmail.com>
2024-10-05 02:20:16 +0500

    add loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,6 +44,21 @@ const Home = () => {
     setCriteria(tab);
   };
 
+  const loader = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+      <circle cx={4} cy={12} r={3} fill="currentColor">
+        <animate id="svgSpinners3DotsScale0" attributeName="r" begin="0;svgSpinners3DotsScale1.end-0.25s" dur="0.75s" values="3;.2;3" />
+      </circle>
+      <circle cx={12} cy={12} r={3} fill="currentColor">
+        <animate attributeName="r" begin="svgSpinners3DotsScale0.end-0.6s" dur="0.75s" values="3;.2;3" />
+      </circle>
+      <circle cx={20} cy={12} r={3} fill="currentColor">
+        <animate id="svgSpinners3DotsScale1" attributeName="r" begin="svgSpinners3DotsScale0.end-0.45s" dur="0.75s" values="3;.2;3" />
+      </circle>
+    </svg>
+  );
+
+
   return (
     <div className={styles.container}>
       <div className={styles.tabs}>
@@ -58,7 +73,7 @@ const Home = () => {
         ))}
       </div>
       <div className={styles.canvas}>
-        {loading && <p>Loading...</p>}
+        {loading && <p><span className='flex justify-center items-center text-black mt-[100px] text-5xl'>{loader()}</span></p>}
         {error && <p className={styles.error}>{error}</p>}
         {!loading && !error && (
           <UserCanvas users={users} />
